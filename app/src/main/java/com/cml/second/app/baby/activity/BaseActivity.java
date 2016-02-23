b85f0e9ef96a4ec96e08862cfe15ccc037d8eb72
menglin.chen <menglin.chen>
2016-02-23 16:29:06 +0800

    添加菜单

---
+++ app/src/main/java/com/cml/second/app/baby/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.cml.second.app.baby.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cmlBeliever on 2016/2/23.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
