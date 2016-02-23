3b865edbe42fe5295da98b5c00eede2e0e26a178
menglin.chen <menglin.chen>
2016-02-23 15:59:28 +0800

    test

--- app/src/main/java/com/cml/second/app/baby/widget/MenuLauncherView.java
+++ app/src/main/java/com/cml/second/app/baby/widget/MenuLauncherView.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PointF;
 import android.os.Build;
 import android.util.AttributeSet;
@@ -23,7 +22,8 @@ public class MenuLauncherView extends View {
 
     private static final String TAG = MenuLauncherView.class.getSimpleName();
 
-    private float launcherPadGap = 200;
+    private float launcherPadGap = 100;
+    private float launcherRadius = 30;
     private boolean isLaunching;
 
     /**
@@ -79,6 +79,7 @@ public class MenuLauncherView extends View {
         launcherViewPaint.setColor(launcherViewColor);
 
         launcherPadGap = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, launcherPadGap, getContext().getResources().getDisplayMetrics());
+        launcherRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, launcherRadius, getContext().getResources().getDisplayMetrics());
     }
 
 
@@ -98,13 +99,14 @@ public class MenuLauncherView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (isLaunching) {
+        if (isLaunching || event.getY() < getHeight() / 2) {
             return false;
         }
+        touchPoint = new PointF(event.getX(), event.getY());
+        boundCheck();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
-                touchPoint = new PointF(event.getX(), event.getY());
                 invalidate();
                 return true;
             case MotionEvent.ACTION_UP:
@@ -115,6 +117,27 @@ public class MenuLauncherView extends View {
         }
 
         return super.onTouchEvent(event);
+    }
+
+    /**
+     * 移动范围检查
+     *
+     * @return
+     */
+    private void boundCheck() {
+        if (touchPoint.x < launcherRadius) {
+            touchPoint.x = launcherRadius;
+        } else if (touchPoint.x > getWidth() - launcherRadius) {
+            touchPoint.x = getWidth() - launcherRadius;
+        }
+
+        if (touchPoint.y + launcherRadius > getHeight()) {
+            touchPoint.y = getHeight() - launcherRadius;
+        } else if (touchPoint.y < getHeight() - launcherPadGap) {
+            touchPoint.y = getHeight() - launcherPadGap + launcherRadius;
+        }
+
+//        return ((touchPoint.x >= launcherRidus && touchPoint.x + launcherRidus <= getWidth()) && (touchPoint.y + launcherRidus <= getHeight() && touchPoint.y >= getHeight() - launcherPadGap));
     }
 
     private ValueAnimator.AnimatorUpdateListener updateListener = new ValueAnimator.AnimatorUpdateListener() {
@@ -133,7 +156,6 @@ public class MenuLauncherView extends View {
         animator = new ValueAnimator();
         animator.addUpdateListener(updateListener);
         animator.setDuration(250);
-//        animator.setFloatValues(touchPoint.y, 0);
         animator.setObjectValues(touchPoint, new PointF(touchPoint.x, 0));
         animator.setEvaluator(new BezierEvaluator());
         animator.addListener(new Animator.AnimatorListener() {
@@ -160,23 +182,11 @@ public class MenuLauncherView extends View {
         animator.start();
     }
 
-    private double getAngle(PointF center) {
-
-        //计算三条边长
-        double lenLeft = Math.sqrt(Math.pow(center.x, 2) + Math.pow(center.y - (getMeasuredHeight() - launcherPadGap), 2));
-        double lenRight = Math.sqrt(Math.pow(getMeasuredWidth() - center.x, 2) + Math.pow(center.y - (getMeasuredHeight() - launcherPadGap), 2));
-        double lenCenter = getMeasuredWidth();
-
-        double value = (lenLeft * lenLeft + lenLeft * lenRight - lenCenter * lenCenter) / 2 * lenLeft * lenRight;
-        return 0;
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         int width = getMeasuredWidth();
         int height = getMeasuredHeight();
 
-        //折现
 
         if (!isLaunching) {
             if (touchPoint == null) {
@@ -187,12 +197,12 @@ public class MenuLauncherView extends View {
                 canvas.drawLine(0, height - launcherPadGap, touchPoint.x, touchPoint.y, launcherPadPaint);
                 canvas.drawLine(touchPoint.x, touchPoint.y, width, height - launcherPadGap, launcherPadPaint);
                 //画发射器
-                canvas.drawCircle(touchPoint.x, touchPoint.y, 100, launcherViewPaint);
+                canvas.drawCircle(touchPoint.x, touchPoint.y, launcherRadius, launcherViewPaint);
             }
 
-        } else {
+        } else {  //正在执行动画
             //画发射器
-            canvas.drawCircle(launchingPoint.x, launchingPoint.y, 100, launcherViewPaint);
+            canvas.drawCircle(launchingPoint.x, launchingPoint.y, launcherRadius, launcherViewPaint);
             if (launchingPoint.y <= height - launcherPadGap) {
                 canvas.drawLine(0, height - launcherPadGap, width, height - launcherPadGap, launcherPadPaint);
             } else {
@@ -201,104 +211,8 @@ public class MenuLauncherView extends View {
                 canvas.drawLine(launchingPoint.x, launchingPoint.y, width, height - launcherPadGap, launcherPadPaint);
             }
         }
-
-
-//        float r = 100;
-//        for (int j = 0; j <= 360; j++) {
-//            double x = r * Math.cos(Math.PI / 45 * j) + 320 / 2;
-//            double y = -r * Math.sin(Math.PI / 45 * j) + 400 / 4;
-//        }
-
-//        drawLove(canvas);
-//
-//        float r = 100;
-//        for (int i = 1; i <= 360; i++) {
-//
-//            float startX = (float) getX(r, Math.PI * i);
-//            float startY = (float) getY(r, Math.PI * i);
-//            i++;
-//            float endX = (float) getX(r, Math.PI * i);
-//            float endY = (float) getY(r, Math.PI * i);
-//            canvas.drawLine(startX, startY, endX, endY, launcherPadPaint);
-//        }
-//
-
-//        canvas.drawLine(0, height - launcherPadGap, touchPoint.x, touchPoint.y, launcherPadPaint);
-//        canvas.drawLine(touchPoint.x, touchPoint.y, width, height - launcherPadGap, launcherPadPaint);
-
-
     }
 
-    private void drawLove(Canvas canvas) {
-        //(17*(x^2))-(16*abs(x)*y)+(17*(y^2))<255 x(-5,5) y(-5,5) (心形函数方程式)
-        int loveWidth = 500;//心型宽度，必须是偶数
-        int oneLine = loveWidth / 2;//一条轴长度
-
-        float scale = oneLine / 5f;//实际坐标比上方程式坐标，倍数
-
-        for (int i = 0; i < oneLine; i++) {
-            for (int j = 0; j < oneLine; j++) {
-
-                //根据表达式xy的范围，所以要把坐标系的范围也缩小
-                float xf = i / scale;
-                float yf = j / scale;
-
-                if ((17 * Math.pow(xf, 2) - 16 * Math.abs(xf) * yf + 17 * Math.pow(yf, 2)) < 255) {
-
-                    //右上1
-//                  canvas.drawPoint(xf*scale+250,250+yf*scale, paint);
-                    //左下2
-                    canvas.drawPoint(250 - xf * scale, 250 - yf * scale, launcherPadPaint);
-//                  this.postInvalidateDelayed(10);
-
-
-                    //右上1
-//                  canvas.drawPoint(-xf*scale+250,250+yf*scale, paint);
-                    //右下2
-                    canvas.drawPoint(250 + xf * scale, 250 - yf * scale, launcherPadPaint);
-//                  this.postInvalidateDelayed(10);
-
-//                  Log.i("TAG", "xf-->"+(xf*scale+250)+"yf-->"+(250-yf*scale));
-                }
-
-                if ((17 * Math.pow(xf, 2) - 16 * Math.abs(xf) * (-yf) + 17 * Math.pow(yf, 2)) < 255) {
-
-                    //左上2
-                    canvas.drawPoint(250 - xf * scale, 250 + yf * scale, launcherPadPaint);
-                    //右下 1
-//                  canvas.drawPoint(250+xf*scale,250-yf*scale, paint);
-
-//                  this.postInvalidateDelayed(10);
-
-
-                    //左上2
-                    canvas.drawPoint(250 + xf * scale, 250 + yf * scale, launcherPadPaint);
-                    //右下 1
-//                  canvas.drawPoint(250-xf*scale,250-yf*scale, paint);
-
-//                  this.postInvalidateDelayed(10);
-
-                }
-            }
-        }//end for
-
-    }
-
-    private double getX(float r, double t) {//由弧度得到 X 坐标
-        return 100 + r * (16 * Math.pow(Math.sin(t), 3));
-    }
-
-    private double getY(float r, double t) {//由弧度得到 Y 坐标
-        return 50 - r * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
-    }
-
-    private void drawLauncherLine(Canvas canvas, int width, int height) {
-        Path launchPadPath = new Path();
-        launchPadPath.reset();
-        launchPadPath.moveTo(0, height - launcherPadGap);
-        launchPadPath.quadTo(touchPoint.x, touchPoint.y, width, height - launcherPadGap);
-        canvas.drawPath(launchPadPath, launcherPadPaint);
-    }
 
     class BezierEvaluator implements TypeEvaluator<PointF> {
 
@@ -308,7 +222,6 @@ public class MenuLauncherView extends View {
             final float t = fraction;
             float oneMinusT = 1.0f - t;
             PointF point = new PointF();
-
 
             PointF point1 = new PointF();
             point1.set(getWidth() * oneMinusT, 0);
