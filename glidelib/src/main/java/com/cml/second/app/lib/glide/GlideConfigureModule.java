7050c3595ad82106baacb49536f8d9baec4184d7
menglin.chen <menglin.chen>
2016-02-24 12:25:40 +0800

    dd

--- glidelib/src/main/java/com/cml/second/app/lib/glide/GlideConfigureModule.java
+++ glidelib/src/main/java/com/cml/second/app/lib/glide/GlideConfigureModule.java
@@ -1,7 +1,23 @@
 package com.cml.second.app.lib.glide;
 
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.module.GlideModule;
+
 /**
  * Created by cmlBeliever on 2016/2/24.
+ * 自定义Glide参数信息
  */
-public class GlideConfigureModule {
+public class GlideConfigureModule implements GlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+
+    }
 }
