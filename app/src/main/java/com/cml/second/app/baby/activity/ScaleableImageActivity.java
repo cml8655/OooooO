231804b6e99834b25057927336b9c7e402242971
menglin.chen <menglin.chen>
2016-08-23 18:33:17 +0800

    图片大图查看功能添加

--- app/src/main/java/com/cml/second/app/baby/activity/ScaleableImageActivity.java
+++ app/src/main/java/com/cml/second/app/baby/activity/ScaleableImageActivity.java
@@ -1,9 +1,11 @@
 package com.cml.second.app.baby.activity;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
 import com.cml.second.app.baby.R;
 import com.cml.second.app.baby.widget.PinchImageView;
@@ -25,10 +27,29 @@ public class ScaleableImageActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_image_scale);
+
+        getWindow().getDecorView().post(new Runnable() {
+            @Override
+            public void run() {
+//                WindowManager.LayoutParams params = (WindowManager.LayoutParams) getWindow().getDecorView().getLayoutParams();
+//                params.screenBrightness = 0.8f;
+//                getWindow().setAttributes(params);
+            }
+        });
+
         ButterKnife.bind(this);
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Toast.makeText(this, "changed:" + newConfig.orientation, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public void finish() {
