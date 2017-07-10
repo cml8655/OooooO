13b3b3c65c6d1d047bfb91983bef7f3890b9e3b2
menglin.chen <menglin.chen>
2017-07-10 16:26:09 +0800

    底部菜单添加

--- app/src/main/java/com/cml/second/app/baby/fragment/SpringAnimationFragment.java
+++ app/src/main/java/com/cml/second/app/baby/fragment/SpringAnimationFragment.java
@@ -1,11 +1,21 @@
 package com.cml.second.app.baby.fragment;
 
+import android.os.Bundle;
 import android.support.animation.SpringAnimation;
 import android.support.animation.SpringForce;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.cml.second.app.baby.R;
+
+import java.lang.reflect.Field;
 
 import butterknife.Bind;
 import butterknife.OnClick;
@@ -17,9 +27,26 @@ import butterknife.OnClick;
 
 public class SpringAnimationFragment extends BaseFragment {
 
+    @Bind(R.id.navigation)
+    android.support.design.widget.BottomNavigationView navigationView;
 
     @Bind(R.id.view1)
     View animView;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        BottomNavigationViewHelper.disableShiftMode(navigationView);
+        navigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Toast.makeText(getContext(), "menuClicked:" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+    }
 
     @OnClick(R.id.view1)
     public void onViewClieck() {
@@ -64,5 +91,29 @@ public class SpringAnimationFragment extends BaseFragment {
     @Override
     public int getTitle() {
         return R.string.title_springanimnation;
+    }
+
+    public static class BottomNavigationViewHelper {
+        public static void disableShiftMode(BottomNavigationView view) {
+            BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+            try {
+                Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+                shiftingMode.setAccessible(true);
+                shiftingMode.setBoolean(menuView, false);
+                shiftingMode.setAccessible(false);
+                for (int i = 0; i < menuView.getChildCount(); i++) {
+                    BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                    //noinspection RestrictedApi
+                    item.setShiftingMode(false);
+                    // set once again checked value, so view will be updated
+                    //noinspection RestrictedApi
+                    item.setChecked(item.getItemData().isChecked());
+                }
+            } catch (NoSuchFieldException e) {
+                Log.e("BNVHelper", "Unable to get shift mode field", e);
+            } catch (IllegalAccessException e) {
+                Log.e("BNVHelper", "Unable to change value of shift mode", e);
+            }
+        }
     }
 }
