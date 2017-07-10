8daa8ffedab1a1e0b5648046dd18cc9468e98fea
menglin.chen <menglin.chen>
2017-07-10 15:42:56 +0800

    添加缩放动画

--- app/src/main/java/com/cml/second/app/baby/fragment/SpringAnimationFragment.java
+++ app/src/main/java/com/cml/second/app/baby/fragment/SpringAnimationFragment.java
@@ -3,11 +3,13 @@ package com.cml.second.app.baby.fragment;
 import android.support.animation.SpringAnimation;
 import android.support.animation.SpringForce;
 import android.view.View;
+import android.widget.Toast;
 
 import com.cml.second.app.baby.R;
 
 import butterknife.Bind;
 import butterknife.OnClick;
+
 
 /**
  * Created by cmlBeliever on 2017/7/10.
@@ -19,6 +21,11 @@ public class SpringAnimationFragment extends BaseFragment {
     @Bind(R.id.view1)
     View animView;
 
+    @OnClick(R.id.view1)
+    public void onViewClieck() {
+        Toast.makeText(getContext(), "click", Toast.LENGTH_SHORT).show();
+    }
+
     @OnClick(R.id.startBtn)
     public void startAnim(View view) {
         animView.setY(0);
@@ -26,9 +33,27 @@ public class SpringAnimationFragment extends BaseFragment {
         SpringForce force = new SpringForce(500).setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY).setStiffness(SpringForce.STIFFNESS_LOW);
         SpringAnimation animation = new SpringAnimation(animView, SpringAnimation.TRANSLATION_Y).setSpring(force);
         animation.start();
+
         SpringForce force2 = new SpringForce(500).setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY).setStiffness(SpringForce.STIFFNESS_LOW);
         SpringAnimation animation2 = new SpringAnimation(animView, SpringAnimation.TRANSLATION_X).setSpring(force2);
         animation2.start();
+
+        SpringForce force3 = new SpringForce(3).setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY).setStiffness(SpringForce.STIFFNESS_LOW);
+        SpringAnimation animation3 = new SpringAnimation(animView, SpringAnimation.SCALE_X).setSpring(force3);
+        animation3.start();
+    }
+
+    @OnClick(R.id.startBtn2)
+    public void startAnim2(View view) {
+        animView.setScaleX(0);
+        animView.setScaleY(0);
+        SpringForce force2 = new SpringForce(2).setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY).setStiffness(SpringForce.STIFFNESS_LOW);
+        SpringAnimation animation2 = new SpringAnimation(animView, SpringAnimation.SCALE_Y).setSpring(force2);
+        animation2.start();
+
+        SpringForce force3 = new SpringForce(2).setDampingRatio(SpringForce.DAMPING_RATIO_HIGH_BOUNCY).setStiffness(SpringForce.STIFFNESS_LOW);
+        SpringAnimation animation3 = new SpringAnimation(animView, SpringAnimation.SCALE_X).setSpring(force3);
+        animation3.start();
     }
 
     @Override
